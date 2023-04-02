0f5ccbc98bf725fbb75592d5d6322c6e4221eb0f
Abdulaziz Alsafadi <azeez.h.749@gmail.com>
2023-04-02 12:00:59 +0300

    adding css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductItemComponent } from './components/product-item/product-item.component';
+import { ProductItemDetailComponent } from './components/product-item-detail/product-item-detail.component';
+import { CartComponent } from './components/cart/cart.component';
+import { ConformationComponent } from './components/conformation/conformation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductItemComponent,
+    ProductItemDetailComponent,
+    CartComponent,
+    ConformationComponent
   ],
   imports: [
     BrowserModule,
